14dbfe08b4f91ad5a4920b4a3c4fc46727bdd47a
Michał Rzeszutko <michal.rzeszutko@outlook.com>
2024-03-17 03:07:04 +0000

    deposit modal

--- packages/nextjs/app/vaults/_components/depositModal.tsx
+++ packages/nextjs/app/vaults/_components/depositModal.tsx
@@ -1,15 +1,37 @@
 "use client";
 
 import { useRef, useState } from "react";
-import { Address } from "~~/components/scaffold-eth";
+import { parseEther } from "viem";
+import { Address, IntegerInput } from "~~/components/scaffold-eth";
+import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 export const DepositModal = ({ address }: { address: string }) => {
     const myModal = useRef<HTMLDialogElement>(null);
     const [amountInput, setAmountInput] = useState("");
+    // const parseEtherFromStringOrBigInt = (value: string | bigint): bigint => {
+    //   if (typeof value === "string") 
+    //   {
+    //     return parseEther(value);
+    //   }
+    //   else
+    //   {
+    //     return value;
+    //   }
+    // }
+    const { writeAsync, isLoading, isMining } = useScaffoldContractWrite({
+        contractName: "StakeLend",
+        functionName: "fillVault",
+        args: [address, parseEther(amountInput)],
+        blockConfirmations: 1,
+        onBlockConfirmation: txnReceipt => {
+          console.log("Transaction blockHash", txnReceipt.blockHash);
+          myModal.current?.close();
+        },
+      });
+
     const handleDeposit = async (event: React.FormEvent) => {
         event.preventDefault();
-        console.log(address);
-        console.log(amountInput);
+        writeAsync();
         return;
       };
 
@@ -25,6 +47,15 @@ export const DepositModal = ({ address }: { address: string }) => {
             <div className="modal-action">
             <Address address={address} />
             <form onSubmit={handleDeposit} className="flex items-center justify-end mb-5 space-x-3 mx-5">
+
+              {/* <IntegerInput
+                value={amountInput}
+                onChange={updatedAmount => {
+                    setAmountInput(updatedAmount);
+                }}
+                placeholder="value (wei)"
+              /> */}
+
                 <input
                     className="border-primary bg-base-100 text-base-content p-2 mr-2 w-full rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-accent"
                     type="text"
@@ -32,7 +63,7 @@ export const DepositModal = ({ address }: { address: string }) => {
                     placeholder="Enter amount"
                     onChange={e => setAmountInput(e.target.value)}
                 />
-                <button className="btn" type="submit">Create deposit</button>
+                <button className="btn" type="submit">Deposit</button>
             </form>
             </div>
         </div>
--- packages/nextjs/app/vaults/page.tsx
+++ packages/nextjs/app/vaults/page.tsx
@@ -43,7 +43,7 @@ export const vaults: Vault[] = [
     status: VaultStatus.DEPOSITING,
   },
   {
-    address: "0x26D2DfdFc9C7af78e60F5a2450a6C2544D59f42A",
+    address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
     requiredAmount: 1231233.43,
     timestamp: 1742158940,
     rewardPercentage: 32424.5545,
@@ -57,15 +57,15 @@ const Vaults: NextPage = () => {
     <>
       {vaults.map((v, i) => {
         return (
-          <div className="card card-compact w-1/2 bg-base-100 shadow-xl">
-          <div className="card-body">
-            <div className="flex">
-            <div className="flex-initial w-32 h-14"><Address address={v.address} /></div>
-            <div className="flex-initial w-32 h-14">{v.requiredAmount} USDC</div>
-            <div className="flex-initial w-32 h-14">{new Date(v.timestamp).toLocaleDateString()}</div>
-            <div className="flex-initial w-32 h-14">{v.rewardPercentage / 100.0} %</div>
-            <div className="flex-initial w-32 h-14">{v.totalDeposit} USDC</div>
-          </div>
+          <div className="card card-compact w-1/2 bg-base-100 shadow-xl" key={i}> 
+            <div className="card-body">
+              <div className="flex">
+              <div className="flex-initial w-32 h-14"><Address address={v.address} /></div>
+              <div className="flex-initial w-32 h-14">{v.requiredAmount} USDC</div>
+              <div className="flex-initial w-32 h-14">{new Date(v.timestamp).toLocaleDateString()}</div>
+              <div className="flex-initial w-32 h-14">{v.rewardPercentage / 100.0} %</div>
+              <div className="flex-initial w-32 h-14">{v.totalDeposit} USDC</div>
+            </div>
             <div className="card-actions justify-end">
               <DepositModal address={v.address}/>
             </div>
